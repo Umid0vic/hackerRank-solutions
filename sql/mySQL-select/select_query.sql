c37554edf64e9e06f470ecdfb06d81af1ac20ff4
Osman Said <said.osman7991@gmail.com>
2022-11-30 23:38:54 +0100

    mySql select

--- sql/mySQL-select/select_query.sql
+++ sql/mySQL-select/select_query.sql
@@ -118,3 +118,36 @@ WHERE LENGTH(CITY) IN (
   FROM STATION
 )
 ORDER BY LENGTH(CITY) DESC, CITY ASC LIMIT 2;
+
+
+/*      12 - WEATHER OBSERVATION STATION 6
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+Your result cannot contain duplicates.      */
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN('a', 'e', 'i', 'o', 'u');
+
+
+/*      13 - WEATHER OBSERVATION STATION 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.      */
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) IN('a', 'e', 'i', 'o', 'u');
+
+
+/*      13 - WEATHER OBSERVATION STATION 8
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+as both their first and last characters. Your result cannot contain duplicates.     */
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+    LEFT(CITY,1) IN('a', 'e', 'i', 'o', 'u') 
+    AND RIGHT(CITY,1) IN('a', 'e', 'i', 'o', 'u');
+
+
+/*      13 - WEATHER OBSERVATION STATION 9
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.  */
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN('a', 'e', 'i', 'o', 'u');
