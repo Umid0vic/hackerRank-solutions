0b9cc675f2ab456c53daefcb520e0c9daaa1af52
Osman Said <57035358+Umid0vic@users.noreply.github.com>
2022-12-01 23:19:27 +0100

    mySQL- basic select is solved

--- sql/mySQL/select_query.sql
+++ sql/mySQL/select_query.sql
@@ -136,7 +136,7 @@ FROM STATION
 WHERE RIGHT(CITY,1) IN('a', 'e', 'i', 'o', 'u');
 
 
-/*      13 - WEATHER OBSERVATION STATION 8
+/*      14 - WEATHER OBSERVATION STATION 8
 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
 as both their first and last characters. Your result cannot contain duplicates.     */
 SELECT DISTINCT CITY
@@ -146,8 +146,35 @@ WHERE
     AND RIGHT(CITY,1) IN('a', 'e', 'i', 'o', 'u');
 
 
-/*      13 - WEATHER OBSERVATION STATION 9
+/*      15 - WEATHER OBSERVATION STATION 9
 Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.  */
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LEFT(CITY,1) NOT IN('a', 'e', 'i', 'o', 'u');
+
+
+----------------------------------------------------------------------------
+
+
+/*  The STUDENTS table
+-------------------------------
+          STUDENTS
+Field       -      Type
+-------------------------------
+ID          -       NUMBER
+NAME        -       STRING
+MARKS       -       INTEGER      
+-------------------------------
+
+
+       16 - HIGHER THAN 75 MARKS
+Query the Name of any student in STUDENTS who scored higher than  Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters 
+(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.        */
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3) ASC, ID ASC;
+
+
